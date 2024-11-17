2f7bd6a75a224bfe586eb58324fad2bf1a127b09
REYES, Shan Michael D. <shanmichealdelunareyes07@gmail.com>
2024-11-17 23:26:00 +0800

    Update index.js

--- UDEMY CSS /CREATE A SIMPLE FUNCTION WHICH ACCEPTS MULTIPLE /index.js
+++ UDEMY CSS /CREATE A SIMPLE FUNCTION WHICH ACCEPTS MULTIPLE /index.js
@@ -1,1 +1,10 @@
+function perimeterBox(h, w) {
+    return 2 * h + 2 * w; 
+  }
+  
+  
+  let height = 5;
+  let width = 10;
+  let perimeter = perimeterBox(height, width);  
+  console.log(perimeter);  
 
